10cd91519be3488f9960a0ef2ef2a7365fab6545
Michael <mthomp25@nd.edu>
2018-05-23 15:55:12 -0700

    add Run.m

---
+++ Run.m
@@ -0,0 +1,12 @@
+% AA 273, Spring 2018
+%
+% 5/23/18
+%
+% Final Project
+%
+clear variables
+close all
+
+%% Constants
+RE = 6378.137;          % [km]
+mu = 398600.4415;       % [km^3/s^2] (Montenbruck)
