b1db8e65d318f6d30b803ce28c966ff76b3bbb4d
Michael <mthomp25@nd.edu>
2018-05-28 21:23:19 -0700

    updating EKF predict

--- proj_EKF.m
+++ proj_EKF.m
@@ -2,7 +2,7 @@ function [mu, cov] = proj_EKF(y, mut, covt, Q, R, dt)
 
     % EKF predict
     At = A_t(mut, dt); % TODO: replace with A function
-    mu = At*mut; % no control
+    mu = lin_dyn(mut, dt); %At*mut; % no control
     cov = At*covt*At' + Q;
 
     % EKF update
@@ -13,6 +13,27 @@ function [mu, cov] = proj_EKF(y, mut, covt, Q, R, dt)
     cov = cov - Kt*Ct*cov;
 end
 
+
+function x_tp1 = lin_dyn(x, dt)
+x1=x(1); x2=x(2); x3=x(3); x4=x(4); x5=x(5); 
+x6=x(6); x7=x(7); x8=x(8); x9=x(9); x10=x(10); 
+% TODO, make this not hard-coded
+p = (6378.137+586)*(1-0.01234567^2); %semi-latus rectum of cheif
+
+xdot = [x4;
+    x5;
+    x6;
+    x1*x10^2 *(1 + 2*x7/p) + 2*x10 *(x5-x2*x8/x7);
+    -2*x10 *(x4-x1*x8/x7) + x2*x10^2 *(1-x7/p); 
+    -x7*x10^2 *x3/p;
+    x8;
+    x7*x10^2 *(1-x7/p);
+    x10;
+    -2*x8*x10/x7];
+
+x_tp1 = x + xdot.*dt;
+
+end
 
 function A = A_t(x, dt)
 x1=x(1); x2=x(2); x3=x(3); x4=x(4); x5=x(5); 
